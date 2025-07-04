7b91f930aece6dc8e7cefc7ccff5d3ea3bb0fdc1
Vincent <113535139+snow-nene@users.noreply.github.com>
2025-07-04 14:29:38 +0800

    refactor: delete CreateWebsiteCascade.java

--- src/main/java/com/custmax/officialsite/mapper/CreateWebsiteCascade.java
+++
@@ -1,8 +0,0 @@
-package com.custmax.officialsite.mapper;
-
-import com.custmax.officialsite.entity.user.User;
-import com.custmax.officialsite.entity.website.Website;
-
-public interface CreateWebsiteCascade {
-    int insert(Website website);
-}
